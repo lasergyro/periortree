1ab9abb385228d0aef08fa0eabdf5adb6f71c0a8
lasergyro <31989178+lasergyro@users.noreply.github.com>
2021-03-26 16:02:59 +0000

    initial python packaging done

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,3 @@
 mkdir -p "$PREFIX/include"
 cp -r "$SRC_DIR/include" "$PREFIX/include/periortree"
-python setup.py install --single-version-externally-managed --record record.txt
+$PYTHON -m pip install . -vv
